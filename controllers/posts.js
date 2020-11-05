cff6bfe0bff623c736a1a691876eace942f05fe1
hazem.hemily@gmail.com <hazem.hemily@gmail.com>
2020-11-05 19:32:47 +0200

    finishing api section

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,12 +1,16 @@
+const types = require("mongoose").Types;
+const PostModel = require("../models/post");
+const UserModel = require("../models/user");
 const getPosts = async (req, res) => {
   try {
-    const posts = [];
+    const { id } = req.user;
+    const posts = await PostModel.find();
     res.status(200).json({
       success: true,
-      data: posts,
+      posts,
     });
   } catch (error) {
-    res.status(400).json({
+    res.status(500).json({
       success: false,
       error,
       message: error.message,
@@ -14,6 +18,246 @@ const getPosts = async (req, res) => {
   }
 };
 
+const createPost = async (req, res) => {
+  try {
+    const { id } = req.user;
+    const { text } = req.body;
+    const user = await UserModel.findById(id);
+    const post = {
+      text,
+      name: user.name,
+      avatar: user.avatar,
+      user: user._id,
+    };
+    const newPost = new PostModel(post);
+    await newPost.save();
+    return res.status(201).json({
+      success: true,
+      newPost,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+const getPost = async (req, res) => {
+  try {
+    const {postId} = req.params;
+    const post = await PostModel.findById(postId).populate("user", "-password");
+    if (!post)
+      return res.status(404).json({
+        success: true,
+        message: "post is not found",
+      });
+    return res.status(200).json({
+      success: true,
+      post,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+const deletePost = async (req, res) => {
+  console.log(req.user.id, req.params.id);
+  try {
+    const {postId} = req.params;
+    const userId = req.user.id;
+    const post = await PostModel.findOne({ _id: postId });
+    if (!post)
+      return res.status(404).json({
+        success: true,
+        message: "post is not found",
+      });
+    console.log(userId, "    ", post.user);
+    if (userId == post.user) {
+      await post.remove();
+      return res.status(200).json({
+        success: true,
+        message: "post was deleted successfully ",
+      });
+    } else {
+      return res.status(401).json({
+        success: false,
+        message: "UNAUTHORIZED",
+      });
+    }
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      error: error.message + "from catch",
+    });
+  }
+};
+const updatePost = async (req, res) => {
+  try {
+    const {postId }= req.params.id;
+    const userId = req.user.id;
+    const body = req.body;
+    const post = await PostModel.findById(postId);
+    console.log(post.user);
+    console.log(userId);
+    if (!post)
+      return res.status(404).json({
+        success: true,
+        message: "post is not found",
+      });
+    if (userId == post.user) {
+      for (key in body) {
+        post[key] = body[key];
+      }
+      await post.save();
+      return res.status(200).json({
+        success: true,
+        post,
+      });
+    } else {
+      return res.status(401).json({
+        success: false,
+        message: "UNAUTHORIZED",
+      });
+    }
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+const addLike = async (req, res) => {
+  try {
+    const{ postId} = req.params;
+    const userId = req.user.id;
+    const post = await PostModel.findById(postId).populate("user", "-password");
+    console.log(findObjectIdinArray(post.likes, new types.ObjectId(userId)));
+    let isExist = findObjectIdinArray(post.likes, new types.ObjectId(userId));
+    if (!isExist.exist) post.likes.push({ user: userId });
+    await post.save();
+    return res.status(203).json({
+      success: true,
+      post,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+const unlike = async (req, res) => {
+  try {
+    const {postId} = req.params;
+    const userId = req.user.id;
+    const post = await PostModel.findById(postId).populate("user", "-password");
+    console.log(findObjectIdinArray(post.likes, new types.ObjectId(userId)));
+    let isExist = findObjectIdinArray(post.likes, new types.ObjectId(userId));
+    if (isExist.exist) {
+      post.likes.splice(isExist.index, 1);
+    }
+    await post.save();
+    return res.status(203).json({
+      success: true,
+      post,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+const addComment = async (req, res) => {
+  try {
+    const { text } = req.body;
+    const { id } = req.user;
+    const {postId }= req.params;
+    const post = await PostModel.findById(new types.ObjectId(postId));
+    const { _id, name, avatar } = await UserModel.findById(
+      new types.ObjectId(id)
+    );
+    const comment = {
+      user: _id,
+      text,
+      avatar,
+      name,
+    };
+    post.comments.push(comment);
+    await post.save();
+    return res.status(201).json({
+      success: true,
+      comment,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      error,
+    });
+  }
+};
+const removeComment = async (req, res) => {
+  try {
+    const commentInfo = {
+      index: 0,
+      exist: false,
+    };
+    const { id } = req.user;
+    const { postId, commentId } = req.params;
+    const post = await PostModel.findById(new types.ObjectId(postId));
+    const user = await UserModel.findById(new types.ObjectId(id));
+    const comment = post.comments.find((item, index) => {
+      if (new types.ObjectId(commentId).equals(new types.ObjectId(item._id))) {
+        commentInfo.index = index;
+        commentInfo.exist = true;
+        return true ; 
+      }
+    });
+    console.log(commentInfo, "from delete comment");
+    const isPostPublisher = new types.ObjectId(post.user).equals(
+      new types.ObjectId(user._id)
+    );
+    const isCommentPublisher = new types.ObjectId(comment.user).equals(
+      new types.ObjectId(user._id)
+    );
+    if (isCommentPublisher || isPostPublisher) post.comments.splice(commentInfo.index, 1);
+    await post.save();
+    return res.status(200).json({
+      success: true,
+      message: "comment deleted successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+function findObjectIdinArray(array, obj) {
+  for (let i = 0; i < array.length; i++) {
+    if (obj.equals(new types.ObjectId(array[i].user)))
+      return {
+        exist: true,
+        index: i,
+      };
+  }
+  return {
+    exist: false,
+  };
+}
 module.exports = {
   getPosts,
+  createPost,
+  getPost,
+  deletePost,
+  updatePost,
+  addLike,
+  unlike,
+  addComment,
+  removeComment,
 };
--- routes/posts.js
+++ routes/posts.js
@@ -1,11 +1,55 @@
-const express = require("express") ; 
-const router = express.Router() ; 
-const {getPosts} = require("../controllers/posts") ; 
+const express = require("express");
+const router = express.Router();
+const { authGuard } = require("../utils/authMiddleware");
+const {
+  getPosts,
+  createPost,
+  deletePost,
+  getPost,
+  updatePost,
+  addLike,
+  unlike , 
+  addComment , 
+  removeComment 
+} = require("../controllers/posts");
+// @route get api/posts
+// @desc  Get Posts
+// @access PRIVATE
+router.get("/", authGuard, getPosts);
 
+//@route POST api/posts
+//@desc create post
+//@access PRIVATE
+router.post("/", authGuard, createPost);
 
+// @route get api/posts/:id
+// @desc  Get pst by id
+// @access PRIVATE
+router.get("/:postId", authGuard, getPost);
 
-router.get("/" , getPosts) ; 
-
-
-
-module.exports = router ; 
+// @route DELETE api/posts/:postId
+// @desc  delete post by postId
+// @access PRIVATE
+router.delete("/:postId", authGuard, deletePost);
+// @route PUT  api/posts/:postId
+// @desc  update post by postId
+// @access PRIVATE
+router.put("/:postId", authGuard, updatePost);
+// @route PUT  api/posts/:postId.likes
+// @desc  add likes to post 
+// @access PRIVATE
+router.put("/:postId/likes", authGuard, addLike);
+// @route delete  api/posts/:postId.likes
+// @desc   unlikes to post 
+// @access PRIVATE
+router.delete("/:postId/likes", authGuard, unlike);
+router.put("/:postId", authGuard, updatePost);
+// @route PUT  api/posts/:postId.likes
+// @desc  add likes to post
+// @access PRIVATE
+router.put("/:postId/comments", authGuard, addComment);
+// @route delete  api/posts/:postId.likes
+// @desc   unlikes to post
+// @access PRIVATE
+router.delete("/:postId/comments/:commentId", authGuard, removeComment);
+module.exports = router;
